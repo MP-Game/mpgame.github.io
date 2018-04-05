2996ffa164506d63f6c917e839fb8baeac86b677
MP-GameMP-Game <mp-game@mail.ru>
2018-04-05 21:12:07 +0700

    111
    1111

---
+++ language.php
@@ -0,0 +1,9 @@
+<?php
+$lang["slogan"] = "Догнать и перегнать";
+$lang["wikipedia"] = "Wiki";
+$lang["back"] = "Назад";
+$lang["go"] = "Найти";
+$lang["home"] = "Home";
+$lang["tos"] = "Enter here your Terms of Service text";
+$lang["agree"] = "·C·O·D·E·L·I·S·T·.·C·C·";
+$lang["tos_2"] = "terms of use";
